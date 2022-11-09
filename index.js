5da2c0bf442b30e88036c0e63a90e40b93fbf17c
AmanatUMahmud <amanatmahmud@gmail.com>
2022-11-09 15:38:21 +0600

    /services/:id implemented

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // middle ware
 app.use(cors())
 app.use(express.json());
@@ -17,12 +17,25 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
     const serviceCollection = client.db('paparrazo').collection('services')
     try{
-        // app.get('/',async(req,res)=>)
+        app.get('/',async(req,res)=>{
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const result = await cursor.limit(3).toArray();
+            res.send(result);
+        })
         // geting all services
         app.get('/services',async (req,res)=>{
             const query = {}
             const cursor = serviceCollection.find(query);
             const result = await cursor.toArray();
+            res.send(result);
+        })
+        // for a specific service
+        app.get('/services/:id',async (req,res)=>{
+            const id = req.params.id;
+            console.log(id);
+            const query = {_id:ObjectId(id)};
+            const result = await serviceCollection.findOne(query);
             res.send(result);
         })
     }
